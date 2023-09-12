2bbc95b75c6553735b3fc2c69553b0058b04fb1a
khushbu <khushbusoni475@gmail.com>
2023-09-12 16:59:47 +0530

    [khushbu] Adding exception code for understanding

---
+++ MoodAnalyserDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeOfException.TestArrayIndexOfBoundException();
+            TypeOfException.TestNullReferenceException();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MoodAnalyserDemo/TypeOfException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserDemo
+{
+    internal class TypeOfException
+    {
+        public static void TestArrayIndexOfBoundException()
+        {
+            try
+            {
+                int[] arr = { 10, 20, 30 };
+                Console.WriteLine(arr[3]);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+        }
+
+        public static void TestNullReferenceException()
+        {
+            try
+            {
+                Person p = null;
+                Console.WriteLine(p.Id);
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
